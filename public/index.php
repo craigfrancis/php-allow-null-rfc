f47692d7ef4509556fef7ef0334902bc2c7662e2
Craig Francis <craig@craigfrancis.co.uk>
2022-02-28 10:35:17 +0000

    Update meta description

--- public/index.php
+++ public/index.php
@@ -686,7 +686,7 @@
 	<link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
 
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="description" content="Which PHP functions should accept NULL" />
+	<meta name="description" content="How should NULL be handled?" />
 
 </head>
 <body>
